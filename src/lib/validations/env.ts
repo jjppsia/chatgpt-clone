3cb1dd037c09343b9ca0db4584479d2cbc5209e9
Jp Sia <johnpaulsia4@gmail.com>
2023-05-23 23:18:52 +0800

    chore: update env validation

--- src/lib/validations/env.ts
+++ src/lib/validations/env.ts
@@ -1,8 +1,25 @@
+import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
-const envSchema = z.object({
-  GOOGLE_CLIENT_ID: z.string(),
-  GOOGLE_CLIENT_SECRET: z.string(),
+export const env = createEnv({
+  server: {
+    NEXTAUTH_URL: z.string().url().optional(),
+    NEXTAUTH_SECRET: z.string().min(1),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
+    DATABASE_URL: z.string().min(1),
+    OPENAI_API_KEY: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 })
-
-export const env = envSchema.parse(process.env)
